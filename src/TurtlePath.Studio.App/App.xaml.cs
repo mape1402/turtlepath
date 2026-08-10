1045f79dd58357f35ab2e7a27e80341e5d5c289f
Mario Perez <mperez@elysiumcoding.com>
2026-08-10 17:59:39 -0600

    Add TurtlePath Studio desktop host

---
+++ src/TurtlePath.Studio.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TurtlePath.Studio.App;
+
+public partial class App : Microsoft.Maui.Controls.Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "TurtlePath Studio" };
+	}
+}
---
+++ src/TurtlePath.Studio.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TurtlePath.Studio.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
